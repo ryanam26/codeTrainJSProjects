7a112bc2abf81eb5cdc7b9a39ab2281df467211d
Ryan Morrison <ryanam26@yahoo.com>
2020-05-02 19:40:55 -0400

    Initial Commit

---
+++ theWeatherHere/server.js
@@ -0,0 +1,47 @@
+//The server
+
+const express = require('express');
+const Datastore = require('nedb');
+const fetch = require('node-fetch');
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+//GET response to client to view
+app.get('/api', (request, response) => {
+    //find everythin in the database
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+
+
+//POST lat and lon and time stamp
+app.post('/api', (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
+});
+
+app.get('/weather/:lat/:lon', async (request, response) => {
+    console.log(request.params)
+    const getlat = request.params.lat
+    const getlon = request.params.lon
+    console.log(getlat, getlon)
+    const w_api = "4443a9362e1899f1250bec19128caa2b";
+    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${getlat}&lon=${getlon}&appid=${w_api}`
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    response.json(json);
+});
