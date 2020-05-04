bdd4563b5e5df25a3441f3e9e4bd35962c7a0355
Ryan Morrison <ryanam26@yahoo.com>
2020-05-04 07:40:27 -0400

    add port

--- theWeatherHere/server.js
+++ theWeatherHere/server.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`Strating server at ${port}`)});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
